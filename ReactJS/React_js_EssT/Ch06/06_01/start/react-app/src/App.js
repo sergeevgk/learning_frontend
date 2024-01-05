38bf4b65082ab13595eaed8245fa700826f77619
sergeevgk <gosha199904@gmail.com>
2024-01-06 02:33:07 +0300

    1. Use fetch and loading states to get data from GraphQL. 2. Render data using rendering function and properties. 3. Parse loaded data.

--- ReactJS/React_js_EssT/Ch06/06_01/start/react-app/src/App.js
+++ ReactJS/React_js_EssT/Ch06/06_01/start/react-app/src/App.js
@@ -1,41 +1,93 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-function useInput(initialValue) {
-  const [value, setValue] =
-    useState(initialValue);
-  return [
-    {
-      value,
-      onChange: (e) => setValue(e.target.value)
-    },
-    () => setValue(initialValue)
-  ];
+const query = `query Query {
+  countries {
+    capital
+    code
+    languages {
+      code
+      name
+      native
+    }
+    name
+    emojiU
+  }
+}`;
+
+const queryOptions = {
+	method: "POST",
+	headers: { "Content-Type": "application/json" },
+	body: JSON.stringify({ query }),
+};
+
+function RenderList({ data, renderItem, renderEmpty }) {
+	return !data.length ? (
+		renderEmpty
+	) : (
+		<ul>
+			{data.map((item) => (
+				<li key={item.name}>{renderItem(item)}</li>
+			))}
+		</ul>
+	);
+}
+
+function RenderCountryItem({ name, capital, emojiU, languages }) {
+	const languageList = languages.map((l) => (
+		<li>
+			{l.code} | {l.name}-{l.native}
+		</li>
+	));
+
+	const emojiCodes = emojiU
+		.split(" ")
+		.map((s) => parseInt(s.substring(2), 16));
+
+	return (
+		<>
+			<h1>
+				{name} {String.fromCodePoint(emojiCodes[0], emojiCodes[1])}
+			</h1>
+			<p>Capital is {capital}</p>
+			<ul>{languageList}</ul>
+		</>
+	);
 }
 
 function App() {
-  const [titleProps, resetTitle] = useInput("");
-  const [colorProps, resetColor] =
-    useState("#000000");
-  const submit = (e) => {
-    e.preventDefault();
-    alert(
-      `${titleProps.value}, ${colorProps.value}`
-    );
-    resetTitle();
-    resetColor();
-  };
-  return (
-    <form onSubmit={submit}>
-      <input
-        {...titleProps}
-        type="text"
-        placeholder="color title..."
-      />
-      <input {...colorProps} type="color" />
-      <button>ADD</button>
-    </form>
-  );
+	const [data, setData] = useState(null);
+	const [error, setError] = useState(null);
+	const [loading, setLoading] = useState(false);
+
+	useEffect(() => {
+		setLoading(true);
+		fetch(`https://countries.trevorblades.com/graphql/`, queryOptions)
+			.then((response) => response.json())
+			.then(setData)
+			.then(() => setLoading(false))
+			.catch(setError);
+	}, []);
+
+	if (loading) {
+		return <h1>Loading</h1>;
+	}
+	if (error) {
+		return <pre>{JSON.stringify(error)}</pre>;
+	}
+	if (!data) {
+		return null;
+	}
+
+	return (
+		<div>
+			<RenderList
+				data={data.data.countries}
+				renderEmpty={<p>No data to display in the list.</p>}
+				renderItem={RenderCountryItem}
+			/>
+		</div>
+	);
 }
 
 export default App;
